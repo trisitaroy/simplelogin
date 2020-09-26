5c04908f2ef2103515fe2b39986c2261ce46a0f8
Trisita Roy <trisita.roy@cognizant.com>
2020-09-26 15:34:19 +0530

    first checkin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import {AuthguardGuard} from './authguard.guard'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo :'/login',pathMatch:"full"},
+  {path: 'login', component :LoginComponent},
+  {path: 'dashboard', component :DashboardComponent,canActivate:[AuthguardGuard]}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
